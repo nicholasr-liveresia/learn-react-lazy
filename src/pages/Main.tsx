d49939ee05f011d2f4a7610cce2f45e51ef531d3
nicholasr-liveresia <nicholasr@external.liveresia.com>
2024-05-15 17:16:42 -0300

    first project commit

---
+++ src/pages/Main.tsx
@@ -0,0 +1,71 @@
+interface MainProps {
+  handleShowMore: () => void;
+}
+
+export function Main({ handleShowMore }: MainProps) {
+  return (
+    <div
+      className="p-8 bo border-b"
+      style={{ borderBottom: "1px solid lightgray" }}
+    >
+      <div className="px-4 sm:px-0">
+        <h3 className="text-base font-semibold leading-7 text-gray-900">
+          Applicant Information
+        </h3>
+        <p className="mt-1 max-w-2xl text-sm leading-6 text-gray-500">
+          Personal details and application.
+        </p>
+      </div>
+      <div className="mt-6 border-t border-gray-100">
+        <dl className="divide-y divide-gray-100">
+          <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+            <dt className="text-sm font-medium leading-6 text-gray-900">
+              Full name
+            </dt>
+            <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+              Margot Foster
+            </dd>
+          </div>
+          <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+            <dt className="text-sm font-medium leading-6 text-gray-900">
+              Application for
+            </dt>
+            <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+              Backend Developer
+            </dd>
+          </div>
+          <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+            <dt className="text-sm font-medium leading-6 text-gray-900">
+              Email address
+            </dt>
+            <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+              margotfoster@example.com
+            </dd>
+          </div>
+          <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+            <dt className="text-sm font-medium leading-6 text-gray-900">
+              Salary expectation
+            </dt>
+            <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+              $120,000
+            </dd>
+          </div>
+          <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+            <dt className="text-sm font-medium leading-6 text-gray-900">
+              About
+            </dt>
+            <div className="ml-4 flex-shrink-0">
+              <a
+                href="#"
+                onClick={handleShowMore}
+                className="font-medium text-indigo-600 hover:text-indigo-500"
+              >
+                Show More
+              </a>
+            </div>
+          </div>
+        </dl>
+      </div>
+    </div>
+  );
+}
